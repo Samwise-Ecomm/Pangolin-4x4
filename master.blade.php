81f51cf5c4cd1e4340947bfeb20728ea1b478577
torolat <tjtorola@gmail.com>
2016-01-24 13:53:18 -0800

    implimented menus

--- master.blade.php
+++ master.blade.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-	<title>TITLE</title>
+	<title>P4x4: {{ $page['name'] }}</title>
 
 	<link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Roboto:400,100' type='text/css'>
 	<link rel="stylesheet" href="/css/store/template.css">
@@ -25,14 +25,26 @@
 				</a>
 				<div id="Head-contact">
 					For questions and ordering<br>
-					Call us at <span class="u-active">PHONE NUMBER</span><br>
-					Or email us at <span class="u-active">EMAIL</span>
+					Call us at <span class="u-active">{{ $phone }}</span><br>
+					Or email us at <span class="u-active">{{ $email }}</span>
 				</div>
 			</div>
 		</div>
 		<div id="Head-navBar">
 			<div class="u-contentWrapper">
-				<div id="Head-menu" class="u-inverted">TOP NAVIGATION</div>
+				<div id="Head-menu" class="u-inverted">
+					@foreach ($menu['header'] as $link)
+						@if ($link['id'] == $page['id'])
+							<span class='u-highlight'>{{ $link['name'] }}</span>
+						@else
+							<a href='{{ $link['path'] }}'>{{ $link['name'] }}</a>
+						@endif
+
+						@if ($link !== $menu['header']->last())
+							 | 
+						@endif
+					@endforeach
+				</div>
 				<div id="Head-searchField">
 					<span id="Head-searchIcon" class="fa fa-search"></span>
 					<input type="text" name="search" id="Head-searchInput" placeholder="Search our inventory...">
@@ -43,7 +55,10 @@
 	<div id="Body">
 		<div id="Body-column" class="u-contentWrapper">
 			<div id="Body-content">
-			
+				@if (isset($page['content']))
+					{!! $page['content'] !!}
+				@else
+				@endif
 			</div>
 			<div id="SideNav">
 				<div id="Search"></div>
@@ -55,8 +70,19 @@
 	<div id="Foot">
 		<div id="Foot-blackBar"></div>
 		<div id="Foot-navBar">
-			BOTTOM NAVIGATION<br>
-			COPYRIGHT
+			@foreach ($menu['footer'] as $link)
+				@if ($link['id'] == 1)
+					<span class='u-highlight'>{{ $link['name'] }}</span>
+				@else
+					<a href='{{ $link['path'] }}'>{{ $link['name'] }}</a>
+				@endif
+
+				@if ($link !== $menu['footer']->last())
+					 | 
+				@endif
+			@endforeach
+			<br>
+			{{ $copyright }}
 		</div>
 	</div>
 
