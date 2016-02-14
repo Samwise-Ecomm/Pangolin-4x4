b51d313c7484a0e22cd434e7ae24649aaf9045ce
torolat <tjtorola@gmail.com>
2016-02-13 19:31:46 -0800

    started adding file upload to page customization

--- page.blade.php
+++ page.blade.php
@@ -21,6 +21,42 @@
 	</style>
 </head>
 <body>
+	<script type="x/template" id="upload-template">
+	  <div class="upload-mask" v-show="show" transition="upload">
+	    <div class="upload-wrapper">
+	      <div class="upload-container">
+
+	        <div class="upload-header">
+	          <i class="fa fa-upload"></i> Image Upload<br>
+	          <i class="u-thin">Select image below to insert.</i>
+	        </div>
+	        
+	        <div class="upload-body">
+	          <ul>
+	          	<li v-for="image in images">
+	          		[<span class="fa fa-minus fa-sm u-active"></span>] <span class="u-active">${ image }</span>
+	          	</li>
+	          	<li>
+								<label for="uploader">
+									<span class="u-active">[<span class="fa fa-plus"></span>] Upload new picture</span>
+								</label>
+								<input type="file" name="img" accept="image/bmp,image/gif,image/jpeg,image/png" id="uploader" class="u-hide">
+	          	</li>
+	          </ul>
+	        </div>
+
+	        <div class="upload-footer">
+	          <slot name="footer">
+	            <div class="Button Button--thin Button--dark"
+								@click='show = false'>
+								Cancel</div>
+	          </slot>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	</script>
+
 	<div class="wrapper" :class="(search && !coding)?'search':'noSearch'">
 		<div id="Head">
 			<div id="Head-highlightBar"></div>
@@ -35,11 +71,13 @@
 			</div>
 			<div id="Head-navBar" class="u-unselectable">
 				<div class="u-contentWrapper">
-					<a href="/admin" style="color:white;"><span class="u-active"><i class="fa fa-home"></i> Return to Panel</span></a>
-					&nbsp;|&nbsp;
-					<span @click="save" class="u-active"><i class="fa fa-floppy-o js-save"></i> Save Page</span>
-
-					<span class='u-floatRight'>
+					<div class="u-floatLeft">
+						<a href="/admin" style="color:white;"><span class="u-active"><i class="fa fa-home"></i> Return to Panel</span></a>
+						&nbsp;|&nbsp;
+						<span @click="save" class="u-active"><i class="fa fa-floppy-o js-save"></i> Save Page</span>
+					</div>
+					
+					<div class='u-floatRight'>
 						<span class="u-active" @click="search=!search">
 							<i class="fa fa-fw" :class="search?'fa-check-square-o':'fa-square-o'"></i> Enable Sidebar
 						</span>
@@ -49,7 +87,13 @@
 							<span v-else>Code </span>
 							<i class="fa" :class="coding?'fa-toggle-off':'fa-toggle-on'"></i>
 						</span>
-					</span>
+					</div>
+
+					<div class="u-center">
+						<span class="u-active" v-if="coding" @click="showUpload = !showUpload">
+							<i class="fa fa-image"> Upload/Select Picture</i>	
+						</span>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -82,6 +126,7 @@
 				{{ $page['path'] }}
 			</div>
 		</div>
+		<upload :show.sync="showUpload">
 	</div>
 
 	<script src="/js/ace/ace.js"></script>
@@ -89,6 +134,7 @@
 	<script src="/js/vue-resource.min.js"></script>
 	<script src="/js/store/jquery.min.js"></script>
 	<script src="/js/store/lightbox.min.js"></script>
+
 	<script type="text/javascript">
 	$(document).ready(function() {
     Vue.config.delimiters = ['${', '}']
@@ -101,7 +147,28 @@
     		id: {{ $page['id'] }},
     		search: {{ $page['search'] }},
     		coding: true,
-    		content: ""
+    		content: "",
+    		showUpload: false
+    	},
+
+    	components: {
+    		upload: {
+    			template: '#upload-template',
+    			data: function() {
+    				return {
+    					images: [
+	    					'foo', 'bar', 'baz'
+	    				]
+	    			}
+    			},
+				  props: {
+				    show: {
+				      type: Boolean,
+				      required: true,
+				      twoWay: true    
+				    }
+				  }
+    		}
     	},
 
     	created: function() {
